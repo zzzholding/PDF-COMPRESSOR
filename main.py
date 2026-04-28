b304313399aff04d915595d937fed38397b3fb9f
zzzholding <zamanbekadilov06@gmail.com>
2026-04-28 09:24:05 +0800

    post

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
-import shutil
-import os
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import os 
 
-app = FastAPI
-UPLOAD_FOLDER = ""
+app = FastAPI()
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.get("/")
+def root():
+    return{"message":"Server connect"}
+
+@app.post("/process/")
+async def proc(file:UploadFile = File()):
+    input_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    with open(input_path, "wb") as buffer:
+        buffer.write(await file.read())
+        return FileResponse(
+            path = input_path,
+            media_type='application/octet-stream',
+            filename=file.filename
+        )
+
+
